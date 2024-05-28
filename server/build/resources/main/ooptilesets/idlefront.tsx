142c763dd88e370b39a8a21dc374633c62fba45d
wetooa <derpykidyt@gmail.com>
2024-05-28 18:14:48 +0800

    added game world

---
+++ server/build/resources/main/ooptilesets/idlefront.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="idlefront" tilewidth="32" tileheight="32" tilecount="12" columns="6">
+ <image source="idlefront.png" trans="ff00ff" width="214" height="83"/>
+</tileset>
