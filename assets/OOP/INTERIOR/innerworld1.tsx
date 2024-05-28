4f7b108eec8e7558d8cbda5d38910ff5fc915eed
wetooa <derpykidyt@gmail.com>
2024-05-28 22:57:05 +0800

    nna nay possible game

---
+++ assets/OOP/INTERIOR/innerworld1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="innerworld1" tilewidth="32" tileheight="32" tilecount="3588" columns="45">
+ <image source="../../CS2D/mapexport.png" trans="ff00ff" width="1440" height="1728"/>
+</tileset>
